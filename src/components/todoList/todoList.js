00acb7196d799cc64367ba1db857fa9f95062feb
JuvitaSaini09 <juvitasaini@gmail.com>
2023-02-01 15:23:09 +0530

    docs:added home page

---
+++ src/components/todoList/todoList.js
@@ -0,0 +1,25 @@
+import './todoList.css'
+export const TodoList=()=>{
+    return(
+        <div className="todo-task-list-container">
+        <ul>
+        <li className="todo-task-list">
+            <p>Task 2</p><span><button className="edit-btn"><i className="far fa-edit"></i></button><button className="delete-btn" ><i className="fas fa-trash-alt"></i></button></span>
+            </li>
+
+            <li className="todo-task-list">
+            <p>Task 2</p><span><button className="edit-btn"><i className="far fa-edit"></i></button><button className="delete-btn" ><i className="fas fa-trash-alt"></i></button></span>
+            </li>
+
+            <li className="todo-task-list">
+            <p>Task 2</p><span><button className="edit-btn"><i className="far fa-edit"></i></button><button className="delete-btn" ><i className="fas fa-trash-alt"></i></button></span>
+            </li>
+
+            <li className="todo-task-list">
+            <p>Task 2</p><span><button className="edit-btn"><i className="far fa-edit"></i></button><button className="delete-btn" ><i className="fas fa-trash-alt"></i></button></span>
+            </li>
+
+        </ul>
+    </div>
+    )
+}
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,33 @@
 import React from "react"
+import './home.css'
+import { TodoList } from "../../components/todoList/todoList"
 
 export const Home=()=>{
     return(
         <>
-        <div >
-        <h1>Home Page</h1>
+        <div className="home-body-parent" >
+           <div className="home-body" >
+           <div className="home-body-heading" >
+           <h1>Welcome To pomodoro App</h1>
+            <p>You have 4 tasks for today,all the best !!</p>
+           </div>
+
+            <div className="todo-container" >
+
+                <div className="todo-heading">
+                    <h2>To - Do List</h2>
+                    <div>
+                    <input type="text" className="input-add-task" placeholder="add task here"></input>
+                    <button className="add-task-btn">+</button>
+                    </div>
+                    
+                </div>
+
+             <TodoList />
+
+            </div>
+           </div>
+        
         </div>
         </>
     )
